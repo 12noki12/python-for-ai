69763c537440f2675e93f243a0b088449e36e1bc
disgaguish <wangxuan3683@gmail.com>
2026-01-16 02:46:23 +0900

    My dog class

---
+++ hello.py
@@ -0,0 +1,13 @@
+
+class Dog:
+    def __init__(self, name):
+        self.name = name
+    def bark(self):
+        print("Bark!!!FuCK")
+    def eat(self):
+        return f"{self.name}is eating"
+
+dog1 = Dog("dave")
+dog1.name
+dog1.bark()``
+dog1.eat()

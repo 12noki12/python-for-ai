951fc4858aab795611086834accc5bb88cb0c7b8
disgaguish <wangxuan3683@gmail.com>
2026-01-17 02:12:36 +0900

    made some changes

--- hello.py
+++ hello.py
@@ -5,9 +5,14 @@ class Dog:
     def bark(self):
         print("Bark!!!FuCK")
     def eat(self):
-        return f"{self.name}is eating"
+        return f"{self.name} is eating"
 
 dog1 = Dog("dave")
 dog1.name
-dog1.bark()``
+dog1.bark()
+dog2 = Dog("ruby")
 dog1.eat()
+print(dog1.bark)
+print(dog2.name)
+print(dog1.eat())
+print(dog2.eat())
